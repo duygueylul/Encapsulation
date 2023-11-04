8071c7555d7784c461e621c7e9be79d35efe2513
Duygu Eylül KARATAŞ <96980093+duygueylul@users.noreply.github.com>
2023-11-04 14:34:09 +0300

    Add files via upload

---
+++ GetterSetter.java
@@ -0,0 +1,46 @@
+package Encapsulation;
+
+public class GetterSetter {
+	
+	String isim;
+	String soyisim;
+	int yas;
+	String yasadigi_yer;
+	
+	
+	
+	public void setAd(String ad) {
+		this.isim = isim;
+	}
+	public String getİsim() {
+		return this.isim;
+	}
+	public void setSoyisim(String soyisim) {
+		this.soyisim = soyisim;
+	}
+	public String getSoyisim() {
+		return this.soyisim;
+	}
+	public void setYas(int yas) {
+		this.yas = yas;
+	}
+	public int getYas() {
+		return yas;
+	}
+	
+	
+	
+	public void setYasadigi_yer(String yasadigi_yer) {
+		this.yasadigi_yer = yasadigi_yer;
+	}
+	public String getYasadigi_yer() {
+		return yasadigi_yer;
+	}
+	
+	
+	
+	
+	
+	
+
+}
